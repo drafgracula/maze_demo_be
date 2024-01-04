14ecbfcd2bcb0e462c266bbb864a2f1f236ecf02
Sergey Latyshev <sergyio.od@gmail.com>
2024-01-04 14:53:31 +0300

    feat: moving the maze generation logic to the backend

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"time"
+)
 
-import "rsc.io/quote"
+type Maze struct {
+	Size    int               `json:"size"`
+	Content map[string]string `json:"content"`
+}
+
+func genMapJson(size int) (string, error) {
+	if size <= 0 {
+		return "", fmt.Errorf("Size must be a positive integer")
+	}
+
+	content := make(map[string]string)
+	rand.Seed(time.Now().UnixNano())
+
+	for x := 0; x < size; x++ {
+		for y := 0; y < size; y++ {
+			key := fmt.Sprintf("%d.%d", x, y)
+			// Подстраивайте вероятность по вашему усмотрению
+			if rand.Float64() < 0.3 {
+				content[key] = "1"
+			} else {
+				content[key] = "0"
+			}
+		}
+	}
+
+	maze := Maze{
+		Size:    size,
+		Content: content,
+	}
+
+	jsonString, err := json.Marshal(maze)
+	if err != nil {
+		return "", err
+	}
+
+	return string(jsonString), nil
+}
 
 func main() {
-	fmt.Println(quote.Go())
+	size := 128
+	mazeJSON, err := genMapJson(size)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println("Maze JSON:")
+	fmt.Println(mazeJSON)
 }
